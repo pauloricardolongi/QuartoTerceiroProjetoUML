14d6f6fec3ec633f7dfe333f5fc71ff45ded9fae
pauloricardolongi <pauloricardolongi@gmail.com>
2021-11-27 03:44:29 -0300

    classe Programa texte com toString classe Produto

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,14 @@
+package aplicacao;
+
+import entidades.Produto;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Produto c = new Produto("TV", 1000.00);
+		System.out.println(c);
+
+	}
+
+}
--- src/entidades/Produto.java
+++ src/entidades/Produto.java
@@ -30,6 +30,11 @@ public class Produto {
 	public void setPreco(Double preco) {
 		this.preco = preco;
 	}
+
+	@Override
+	public String toString() {
+		return "Produto [nome=" + nome + ", preco=" + preco + "]";
+	}
 	
 
 }
