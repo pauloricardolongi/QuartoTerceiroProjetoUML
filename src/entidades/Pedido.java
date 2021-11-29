052f6119bbb5d05a36c01639f2e09d65bc5179b3
pauloricardolongi <pauloricardolongi@gmail.com>
2021-11-29 00:41:05 -0300

    Classe Pedido remover get e set da lista

--- src/entidades/Pedido.java
+++ src/entidades/Pedido.java
@@ -25,7 +25,29 @@ public class Pedido {
 		this.estado = estado;
 		this.cliente = cliente;
 	}
-	
-	
+
+	public Date getMomento() {
+		return momento;
+	}
+
+	public void setMomento(Date momento) {
+		this.momento = momento;
+	}
+
+	public EstadoPedido getEstado() {
+		return estado;
+	}
+
+	public void setEstado(EstadoPedido estado) {
+		this.estado = estado;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
 
 }
