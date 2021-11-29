6187ca53ca34ba7a3b4a1ac6f4a39c0d08c03f3c
pauloricardolongi <pauloricardolongi@gmail.com>
2021-11-29 00:31:03 -0300

    classe Pedido: não coloca a coleção INTENS no construtor

--- src/entidades/Pedido.java
+++ src/entidades/Pedido.java
@@ -1,6 +1,8 @@
 package entidades;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import entidades.enums.EstadoPedido;
 
@@ -8,6 +10,21 @@ public class Pedido {
 	
 	private Date momento;
 	private EstadoPedido estado;
+	
+	private Cliente cliente;
+	
+	private List<Item_do_Pedido> itens = new ArrayList<Item_do_Pedido>();
+	
+	public Pedido() {
+		
+	}
+
+	public Pedido(Date momento, EstadoPedido estado, Cliente cliente) {
+		super();
+		this.momento = momento;
+		this.estado = estado;
+		this.cliente = cliente;
+	}
 	
 	
 
